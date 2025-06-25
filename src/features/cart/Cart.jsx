068eb13bd56f84cee37915304f69af4e5efec538
Atefeh Naseri <ati_nas@yahoo.com>
2025-06-25 14:59:58 +0200

    feat(cart): add CRUD to cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,7 +2,9 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../ui/LinkButton';
 import Button from '../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from './cartSlice';
+import EmptyCart from './EmptyCart';
 
 const fakeCart = [
   {
@@ -30,9 +32,15 @@ const fakeCart = [
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
+  // const cart = fakeCart;
+  const cart = useSelector((state) => state.cart.cart);
+  const dispatch = useDispatch();
 
-  const cart = fakeCart;
+  function handleClearCart() {
+    dispatch(clearCart());
+  }
 
+  if (!cart.length) return <EmptyCart />;
   return (
     <div className="px-4 py-3">
       <LinkButton
@@ -52,7 +60,9 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={handleClearCart}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
