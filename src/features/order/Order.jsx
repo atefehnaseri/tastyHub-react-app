ca70c5b571b5223372db63bd3df92d641f64656c
Atefeh Naseri <ati_nas@yahoo.com>
2025-06-30 12:29:42 +0200

    feat(order): add ingredients to order details

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import OrderItem from './OrderItem';
 
@@ -9,6 +9,7 @@ import {
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import { useEffect } from 'react';
 
 const order = {
   id: 'ABCDEF',
@@ -47,8 +48,14 @@ const order = {
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
 
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  //fetching another route data into our current component
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+  }, [fetcher]);
+
+  // Everyone can search for all orders, so for privacy reasons we're gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
     status,
@@ -91,7 +98,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+            isLoadingIngredients={fetcher.state === 'loading'}
+          />
         ))}
       </ul>
 
